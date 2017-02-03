31b0069567913dc18a16f22a06deca6c3b7b5143
shturec <sthrakal@gmail.com>
2017-02-03 12:33:52 +0200

    adapted to changes in data service

--- tags/ScriptingServices/annotations/lib/tags_dao.js
+++ tags/ScriptingServices/annotations/lib/tags_dao.js
@@ -15,6 +15,14 @@ var TagORM = {
 			name: "defaultLabel",
 			dbName: "ANN_DEFAULT_LABEL",
 			type: "String"
+		},{ 
+			name: "description",
+			dbName: "ANN_DESCR",
+			type: "String"
+		},{ 
+			name: "namespace",
+			dbName: "ANN_NS",
+			type: "String"
 		},{
 			name: "uri",
 			dbName: "ANN_URI",
@@ -31,35 +39,37 @@ var TagDAO  = exports.TagDAO = function(orm){
 TagDAO.prototype = Object.create( DAO.prototype );
 
 // Reads a single entity by id, parsed into JSON object 
-TagDAO.prototype.findByTagValue = function(tag) {
+TagDAO.prototype.findByTagValue = function(tag, namespace) {
 
 	this.$log.info('Finding '+this.orm.dbName+' entity with label[' + tag + ']');
 
 	if(tag=== undefined || tag === null){
-		throw new Error('Illegal argument for tag parameter:' + tag);
+		throw new Error('Illegal argument for tag parameter:' + tag + ' and namespace: ' + namespace);
 	}
 
     var connection = this.datasource.getConnection();
     try {
         var entity;
-        var sql = "SELECT * FROM ANN_TAG WHERE ANN_DEFAULT_LABEL = ?";
-     
-        var statement = connection.prepareStatement(sql);
-        statement.setString(1, tag);
-
-        var resultSet = statement.executeQuery();
+        var qb = this.statements.builder().select().from(this.orm.dbName).where('ANN_DEFAULT_LABEL = ?', [this.orm.getProperty('defaultLabel')]);
+        if(namespace!==undefined)
+        	qb.where('ANN_NS = ?', [this.orm.getProperty('namespace')]);
+        var params = {};
+        params['defaultLabel'] = tag;
+        if(namespace)
+        	params['namespace'] = namespace;
+        
+        var resultSet = this.statements.execute(qb, connection, params);
         if (resultSet.next()) {
         	entity = this.createEntity(resultSet);
         	this.$log.info(this.orm.dbName+'[' + entity.id + '] entity with label[' + tag + '] found');
         } else {
-        	this.$log.info(this.orm.dbName+'[' + entity.id + '] entity with label[' + tag + '] not found');
+        	this.$log.info('No '+this.orm.dbName+' records for label[' + tag + '] not found');
         }
         return entity;
     } finally {
         connection.close();
     }
 };
-
 
 exports.get = function(){
 	return new TagDAO(TagORM);
--- tags/ScriptingServices/annotations/svc/tags.js
+++ tags/ScriptingServices/annotations/svc/tags.js
@@ -2,70 +2,32 @@
 /* eslint-env node, dirigible */
 "use strict";
 
+var DataService = require('arestme/data_service').DataService;
+var TagsDataService = function(dao){
+	DataService.call(this, dao, 'Tags Data Service');
+};
+
+TagsDataService.prototype = Object.create(DataService.prototype);
+TagsDataService.prototype.constructor = TagsDataService;
+
 var tagsDAO = require("annotations/lib/tags_dao").get();
-var daoService = require('arestme/data_service').asService(tagsDAO, {}, 'Tags DAO Service');
+var tagsDataService = new TagsDataService(tagsDAO);
 
 var handler = function(context, io){
-	    try{
-	    	//TODO: fix this to a native sql query
-	    	var entities = daoService.dao.list({})
-	    					.filter(function(entity){
-	    						return entity.defaultLabel === context.queryParams.label;
-	    					}); 
-	    	io.response.println(JSON.stringify(entities, null, 2));
-			io.response.setStatus(io.response.OK);
-		} catch(e) {
-    	    var errorCode = io.response.INTERNAL_SERVER_ERROR;
-    	    this.logger.error(e.message, e);
-        	this.sendError(errorCode, errorCode, e.message);
-        	throw e;
-		}		
-	}
-daoService.addResourceHandler('label', 'get', handler, undefined, ["application/json"]);
-daoService.service();
-
-/*var create = function(context, io){
-	var input = io.request.readInputText();
-    var entity = JSON.parse(input);
+	var ns = context.pathParams.ns;
+	var label = context.queryParams.label;
     try{
-		entity[this.dao.getPrimaryKey()] = this.dao.insert(entity, context.queryParams.cascaded);
+    	//TODO: fix this to a native sql query
+    	var entities = tagsDataService.handlersProvider.dao.findByTagValue(label, ns); 
+    	io.response.println(JSON.stringify(entities, null, 2));
 		io.response.setStatus(io.response.OK);
-		io.response.setHeader('Location', $.getRequest().getRequestURL().toString() + '/' + entity[this.dao.getPrimaryKey()]);
 	} catch(e) {
 	    var errorCode = io.response.INTERNAL_SERVER_ERROR;
-	    this.logger.error(errorCode, e.message, e.errContext);
-    	this.sendError(io, errorCode, errorCode, e.message, e.errContext);
+	    this.logger.error(e.message, e);
+    	this.sendError(errorCode, errorCode, e.message);
     	throw e;
-	}	
+	}		
 };
+tagsDataService.addResourceHandler('domains/{ns}', 'get', handler, undefined, ["application/json"]);
 
-Tag.prototype.cfg[""].post = {
-	consumes: ["application/json"],
-	handler: function(context, io){
-		var input = io.request.readInputText();
-	    try{
-	    	var tags = JSON.parse(input);
-	    	if(!Array.isArray(tags)){
-	    		tags = [tags];
-	    	}
-	    	for(var i=0;i<tags.length;i++){
-	    		create.apply(this, [context, io]);
-	    	}
-			io.response.setStatus(io.response.NO_CONTENT);
-		} catch(e) {
-    	    var errorCode = io.response.INTERNAL_SERVER_ERROR;
-    	    this.logger.error(errorCode, e.message, e.errContext);
-        	this.sendError(io, errorCode, errorCode, e.message, e.errContext);
-        	throw e;
-		}		
-	}
-};
-
-
-var tag = new Tag(tagsDAO);	
-
-var request = require("net/http/request");
-var response = require("net/http/response");
-
-tag.service(request, response);
-*/
+tagsDataService.service();
